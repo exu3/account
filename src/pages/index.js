1728d23b7e06253bcddc21657da49e6009ddea4e
Lola <lola@egherman.com>
2021-03-18 17:12:23 -0500

    remove "i'm a volunteer checkbox" (switch to just directing ppl to code endpoint)

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home({ user, token, logIn }) {
       <WelcomeHeader user={user} />
       <Divider />
       <Heading as="h2" size="lg" paddingTop={4}>Update Your Account</Heading>
-      <UserProperties token={token} user={merge(user, changes)} fields={["username", "picture", 'familyName', 'givenName', "displayNameFormat", "pronoun", (user.badges ? "badges" : null), "phoneNumber", "bio", "volunteer", (user.roles.find((role) => role.name === "Volunteer") ? "title" : null), "discord"]} onChange={setChanges} />
+      <UserProperties token={token} user={merge(user, changes)} fields={["username", "picture", 'familyName', 'givenName', "displayNameFormat", "pronoun", (user.badges ? "badges" : null), "phoneNumber", "bio", (user.roles.find((role) => role.name === "Volunteer") ? "title" : null), "discord"]} onChange={setChanges} />
       <Box textAlign="right" marginRight={3}>
         <SubmitUpdates token={token} user={user} changes={changes} required={['username', 'givenName', 'familyName', 'pronoun']} onSubmit={onSubmit} />
         <Box marginTop={3}>
